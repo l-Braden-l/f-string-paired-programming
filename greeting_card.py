b9899b57bbf67f37bc0031925e12116bf8c414c2
Manfred Spitze <155758012+manfredspitze@users.noreply.github.com>
2024-09-10 05:30:32 -0400

    Update greeting_card.py

--- greeting_card.py
+++ greeting_card.py
@@ -7,7 +7,7 @@ Directions:
 
 1. Update the comment block at the top of this script.
 
-2. Use the Python `input( )` function to prompt (ask) the user for three pieces of information:
+2. Use the Python `input( )` function to prompt (ask) the user for three pieces of information for a greeting card you're making:
 
      - the first name of the person the greeting card is for
      - the occasion (e.g., birthday, graduation, etc.)
